2137a3479cda550470458d46514e3ae8fae81449
Mahir KOÇER <mahirkocer@hotmail.com>
2023-05-30 22:02:11 +0300

    rock project is completed.

--- todo-list.js
+++ todo-list.js
@@ -1,12 +1,28 @@
+
+
+let todoList =[];
 function addTodo(){
 
-const todolist =[];
 const inputElement =document.querySelector('.js-input-name');
 
 const name =inputElement.value;
 
-todolist.push(name);
+todoList.push(name);
 inputElement.value ='';
+renderTodoList();
 
 
+}
+function renderTodoList(){
+
+let todolistHTML ='';
+for(let i =0; i<todoList.length; i++){
+  const todo = todoList[i];
+
+const html = `<p>${todo}</p>`;
+todolistHTML += html;
+
+}
+
+document.querySelector('.js-todo-list').innerHTML = todolistHTML;
 }
